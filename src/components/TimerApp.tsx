ec7f95168c7fb33c689b057c03948201d810926c
Ryo <ryo.kunimasa0416@gmail.com>
2025-10-11 12:04:03 +0900

    ボタンコンポーネントの作成

--- src/components/TimerApp.tsx
+++ src/components/TimerApp.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import TimerDisplay from "./TimerDisplay";
+import Controls from "./Controls";
 
 export default function TimerApp() {
   return (
@@ -11,8 +12,9 @@ export default function TimerApp() {
             作業時間
           </CardTitle>
         </CardHeader>
-        <CardContent className="flex justify-center">
+        <CardContent className="flex flex-col items-center gap-6">
           <TimerDisplay minutes={25} seconds={0} />
+          <Controls />
         </CardContent>
       </Card>
     </div>
