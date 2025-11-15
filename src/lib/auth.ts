1e69be219f7c9de005d9c15387627323332709f4
Ryo <ryo.kunimasa0416@gmail.com>
2025-11-15 22:00:11 +0900

    Userモデルを作成

---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+    usePlural: true, //テーブルを複数形で定義する場合に必要
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+});
